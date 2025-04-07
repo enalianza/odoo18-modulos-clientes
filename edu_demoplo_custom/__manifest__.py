6d22abb9fd1d9718da3ca4dec0110d78ff16a808
enalianza <enalianza24@gmail.com>
2025-04-07 19:10:09 +0000

    Primer módulo personalizado: edu_demoplo_custom

---
+++ edu_demoplo_custom/__manifest__.py
@@ -0,0 +1,13 @@
+{
+	'name': 'EDU Demoplo Custom',
+	'version': '1.0',
+	'summary': 'Módulo base personalizado para cliente edu-demoplo',
+	'category': 'Custom',
+	'author': 'enalianza',
+	'depends': ['base'],
+	'data': [
+        	'views/demo_model_views.xml',
+	],
+	'installable': True,
+	'application': True,
+}

52a9ac54dcf9896b4f8b33c479175eb2e6a3722c
enalianza <enalianza24@gmail.com>
2025-04-12 19:22:48 +0000

    Agregar módulo de prueba vacío para validación de Git

---
+++ modulo_ejemplo_vacio/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Módulo Ejemplo Vacío',
+    'version': '1.0',
+    'summary': 'Este módulo no hace nada, es solo una prueba',
+    'category': 'Pruebas',
+    'author': 'enalianza',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True
+}
